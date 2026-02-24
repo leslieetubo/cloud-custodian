b9244bcc4bc4b36866f8e6ed90b8fbdbd188d95c
Leslie Etubo <leslie.etubo@gmail.com>
2026-02-24 10:58:42 -0600

    feat: lint and format

--- tests/terraform/lattice_service_network_detailspec/main.tf
+++ tests/terraform/lattice_service_network_detailspec/main.tf
@@ -2,6 +2,6 @@ resource "aws_vpclattice_service_network" "test" {
   name      = "test-lattice-network"
   auth_type = "AWS_IAM"
   tags = {
-    PolicyTestASV = "PolicyTestASVValue"
+    TestServiceNetwork = "TestServiceNetworkValue"
   }
 }
